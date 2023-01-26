fe12a58345e8841ba51d7b7eb6d3a8de901a08bb
MagdielPaulo <81927049+MagdielPaulo@users.noreply.github.com>
2023-01-26 19:36:19 -0300

    First Commit Upload

---
+++ sintaxe-basica/src/Programa.java
@@ -0,0 +1,10 @@
+
+public class Programa {
+	
+	public static void main (String [] args) {
+		System.out.println("Olá Mundo");
+		
+		String palavra = "Teste de Commit";
+		System.out.println(palavra);
+	}
+}
