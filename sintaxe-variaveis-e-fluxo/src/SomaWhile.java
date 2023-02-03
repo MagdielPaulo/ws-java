5e4f1fc854f221a0f51b6fea7353ea596cf20ef6
MagdielPaulo <81927049+MagdielPaulo@users.noreply.github.com>
2023-02-03 10:09:46 -0300

    Commit ++

---
+++ sintaxe-variaveis-e-fluxo/src/SomaWhile.java
@@ -0,0 +1,15 @@
+
+public class SomaWhile {
+
+	public static void main(String[] args) {
+		int contador = 0;
+		int total = 0;
+		
+		while(contador <= 10) {
+			total += contador;
+			contador++;
+		}
+		
+		System.out.println(total);
+	}
+}
